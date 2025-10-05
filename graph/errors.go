1c039dcf9bba3896532508e12b72832dbda59b5e
tolstovrob <tolstovrob@gmail.com>
2025-10-05 22:19:14 +0400

    feat(graph): add error handlers

--- graph/errors.go
+++ graph/errors.go
@@ -1,1 +1,19 @@
 package graph
+
+import "fmt"
+
+func ThrowNodeWithKeyExists(key TKey) error {
+	return fmt.Errorf("Node with key %v already exists", key)
+}
+
+func ThrowNodeWithKeyNotExists(key TKey) error {
+	return fmt.Errorf("Node with key %v not exists", key)
+}
+
+func ThrowEdgeWithKeyExists(key TKey) error {
+	return fmt.Errorf("Edge with key %v already exists", key)
+}
+
+func ThrowEdgeWithKeyNotExists(key TKey) error {
+	return fmt.Errorf("Edge with key %v not exists", key)
+}
